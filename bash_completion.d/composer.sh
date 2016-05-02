8233dffb2451dfb62d30a20361240528e7739147
Peter Schultz <peter.schultz@classmarkets.com>
2016-05-02 18:23:00 +0200

    update composer completion

--- bash_completion.d/composer.sh
+++ bash_completion.d/composer.sh
@@ -1,77 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 #
-# https://github.com/jaytaph/SFConsole
+# bash completion support for symfony2 console
 #
-# Symfony2 App/Console autocompletion (commands and arguments only)
-# Copyright (c) 2014, Joshua Thijssen
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-# 
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-# 
-# * Redistributions in binary form must reproduce the above copyright notice, this
-#   list of conditions and the following disclaimer in the documentation and/or
-#   other materials provided with the distribution.
-# 
-# * Neither the name of the {organization} nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# 
-# Usable for both bash and zsh (probably)
-#
-# Usage:
-#    Load the script (or add to your .bashrc)
-#
-#           source ./console_completion.sh
-#
-#    Autocomplete:
-#
-#           ./app/console <TAB>
-#
-#   Will autocomplete both commands and its arguments.
-#
+# Copyright (C) 2011 Matthieu Bontemps <matthieu@knplabs.com>
+# Distributed under the GNU General Public License, version 2.0.
 
-if [[ -n ${ZSH_VERSION-} ]]; then
-    autoload -U +X bashcompinit && bashcompinit
-fi
-
-_complete_sf2_app_console() {
-    local cur
-
+_complete_sf2_app_console()
+{
+    local cur prev script
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    script="${COMP_WORDS[0]}"
 
-    # Assume first word is the actual app/console command
-    console="${COMP_WORDS[0]}"
-
-    if [[ ${COMP_CWORD} == 1 ]] ; then
-        # No command found, return the list of available commands
-        cmds=` ${console} --no-ansi | sed -n -e '/^Available commands/,//p' | grep '^  ' | awk '{ print $1 }'  `
-    else
-        # Commands found, parse options
-        cmds=` ${console} ${COMP_WORDS[1]} --no-ansi --help | sed -n -e '/^Options/,/^$/p' | grep '^ ' | awk '{ print $1 }' `
-    fi
-
-    COMPREPLY=( $(compgen -W "${cmds}" -- ${cur}) )
-    return 0
+    if [[ ${cur} == -* ]] ; then
+        PHP=$(cat <<'HEREDOC'
+array_shift($argv);
+$script = array_shift($argv);
+$command = '';
+foreach ($argv as $v) {
+    if (0 !== strpos($v, '-')) {
+        $command = $v;
+        break;
+    }
 }
 
-export COMP_WORDBREAKS="\ \"\\'><=;|&("
+$xmlHelp = shell_exec($script.' help --xml '.$command);
+$options = array();
+if (!$xml = @simplexml_load_string($xmlHelp)) {
+    exit(0);
+}
+foreach ($xml->xpath('/command/options/option') as $option) {
+    $options[] = (string) $option['name'];
+}
+
+echo implode(' ', $options);
+HEREDOC
+)
+
+        args=$(printf "%s " "${COMP_WORDS[@]}")
+        options=$($(which php) -r "$PHP" ${args});
+        COMPREPLY=($(compgen -W "${options}" -- ${cur}))
+
+        return 0
+    fi
+
+    commands=$(${script} list --raw | sed -E 's/(([^ ]+ )).*/\1/')
+    COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
+
+    return 0;
+}
+
+COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
+
 complete -F _complete_sf2_app_console bin/console
+complete -F _complete_sf2_app_console bin/clickout-console
 complete -F _complete_sf2_app_console cmp
 complete -F _complete_sf2_app_console composer.phar
