3ed0bc10b0f6c7ba03e06789120cffe08373efd3
Peter Schultz <peter.schultz@classmarkets.com>
2022-01-25 17:33:43 +0100

    nvim: change scheme style to night

--- nvim/lua/local/theme.lua
+++ nvim/lua/local/theme.lua
@@ -9,23 +9,12 @@ end
 
 M.config = function()
     -- vim.g.tokyonight_style = 'day'
-    -- vim.g.tokyonight_style = 'night'
-    vim.g.tokyonight_style = 'storm'
+    -- vim.g.tokyonight_style = 'storm'
+    vim.g.tokyonight_style = 'night'
     vim.g.tokyonight_hide_inactive_statusline = true
 
     -- darker sidebars
     vim.g.tokyonight_sidebars = {'packer', 'terminal'}
-
-    --local colors = require('tokyonight.colors')
-    --local c = colors.setup(nil)
-
-    --[[
-    vim.g.tokyonight_colors = {
-        -- https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/theme.lua
-        -- https://github.com/folke/tokyonight.nvim/blob/main/lua/tokyonight/colors.lua
-        CursorLine = { bg = c.bg_highlight }
-    }
-    --]]
 
     vim.cmd('colorscheme tokyonight')
 
